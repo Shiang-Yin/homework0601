802303eacc2735a6ab8417e316af632e7d3fdcf0
Shiang-Yin <ua011413@gmail.com>
2024-06-01 21:56:47 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  
+  <div class="header">
+    <Header />
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+.header {
+  width: 100dvh;
+  height: 100dvh;
+
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+export default{
+
+}
+</script>
+
+<template>
+
+<div class="headerArea">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+</div>
+
+</template>
+
+
+<style scoped lang="scss">
+</style>
